db551cee92410db69fe370474e35b50f26593827
MohammedSayed07 <mohammed.s.hegazi@gmail.com>
2023-12-01 20:06:24 +0200

    cleared bootstrap

--- views/register.view.php
+++ views/register.view.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
 
-    <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="modal modal-sheet d-block bg-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSignin">
